e48c2e582042094632a77428bbe69dc4edf63615
officekiev\y.tishchenko <y.tishchenko@temabit.com>
2020-07-09 09:40:36 +0300

    Add models

---
+++ BankOfDotNet/BankOfDotNet.WebAPI/Models/Customer.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BankOfDotNet.WebAPI.Models
+{
+    public class Customer
+    {
+        public long Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
